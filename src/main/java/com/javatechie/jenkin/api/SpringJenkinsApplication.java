13487af34f4d01d062001e6306dd9eb1fb3abfbb
basahota <techbasanta757@gmail.com>
2018-12-23 00:01:45 +0530

    added readme file

--- src/main/java/com/javatechie/jenkin/api/SpringJenkinsApplication.java
+++ src/main/java/com/javatechie/jenkin/api/SpringJenkinsApplication.java
@@ -1,24 +1,21 @@
 package com.javatechie.jenkin.api;
 
-import javax.annotation.PostConstruct;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringJenkinsApplication {
 
-	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
 
-	@PostConstruct
-	public void intt() {
-		logger.info("Application started...");
+	@GetMapping("/ping")
+	public String message() {
+		return "Wao!! Application Deployed successfully in SAP Cloud..";
 	}
 
 	public static void main(String[] args) {
-		logger.info("Application executed...");
 		SpringApplication.run(SpringJenkinsApplication.class, args);
 	}
 
--- src/test/java/com/javatechie/jenkin/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/javatechie/jenkin/api/SpringJenkinsApplicationTests.java
@@ -13,6 +13,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpringJenkinsApplicationTests {
 
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
 
 	@Test
 	public void contextLoads() {
