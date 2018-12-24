bb0156f4a416043f0439f509e185aa5c659f3e37
basahota <techbasanta757@gmail.com>
2018-12-24 16:05:01 +0530

    fail commit

--- src/test/java/com/javatechie/jenkin/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/javatechie/jenkin/api/SpringJenkinsApplicationTests.java
@@ -16,7 +16,7 @@ public class SpringJenkinsApplicationTests {
 	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
 
 	@Test
-	public void contextLoads() {
+	public  contextLoads() {
 		logger.info("Test case executing...");
 		assertEquals(true, true);
 	}
