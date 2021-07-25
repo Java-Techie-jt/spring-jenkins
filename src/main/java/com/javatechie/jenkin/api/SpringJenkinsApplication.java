b34a76107bbc3c25cf95d940e11d8f1235f3d844
prasadm1992 <87922519+prasadm1992@users.noreply.github.com>
2021-07-25 20:19:57 +0800

    bug fix
    bug fix

--- src/main/java/com/javatechie/jenkin/api/SpringJenkinsApplication.java
+++ src/main/java/com/javatechie/jenkin/api/SpringJenkinsApplication.java
@@ -12,7 +12,7 @@ public class SpringJenkinsApplication {
 
 	@GetMapping("/ping")
 	public String message() {
-		System.out.println("hello world 2")
+		System.out.println("hello world 2");
 		return "Wao!! Application Deployed successfully in SAP Cloud..";
 	}
 
